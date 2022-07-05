1f399d2b60064c6034632f0255465c9539046704
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-07-04 17:43:16 -0700

    Create one pokemon entry component

---
+++ src/components/PokemonEntry.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PokemonEntry = () => {
+    return (
+        <span>
+            <img src="#" alt="PokemonEntry icon goes here" />
+            <div>name of pokemon</div>
+            <div>pokedex #</div>
+        </span>
+    );
+};
+
+export default PokemonEntry;
