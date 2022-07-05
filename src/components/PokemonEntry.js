9699c65af612e0959e7576e81c22756142b460a4
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-07-04 18:25:40 -0700

    Tweak alt text

--- src/components/PokemonEntry.js
+++ src/components/PokemonEntry.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PokemonEntry = () => {
     return (
         <div className="pkmn-entry">
-            <img src="#" alt="PokemonEntry icon goes here" />
+            <img src="#" alt="icon goes here" />
             <div className="pkmn-name">name of pokemon</div>
             <div className="pkmn-id">pokedex #</div>
         </div>
