827d373828f5cd91c7d696cd0777654d8eb3475a
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-07-05 19:54:44 -0700

    Update SelectedIndex when entry is clicked

--- src/components/PokemonEntry.js
+++ src/components/PokemonEntry.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const PokemonEntry = ({pokemon}) => {
+const PokemonEntry = ({pokemon, updateSelected}) => {
+
     return (
-        <div className="pkmn-entry">
+        <div className="pkmn-entry" onClick={updateSelected}>
             <img src={pokemon.smallIcon} alt="icon goes here" />
             <div className="pkmn-name">{pokemon.name}</div>
             <div className="pkmn-id">{pokemon.id}</div>
