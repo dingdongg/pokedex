a228537dc970b37ada27311973f09490655db1f4
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-07-05 19:33:58 -0700

    Edit name of pokemon object attributes

--- src/components/PokemonEntry.js
+++ src/components/PokemonEntry.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PokemonEntry = ({pokemon}) => {
     return (
         <div className="pkmn-entry">
-            <img src={pokemon.icon} alt="icon goes here" />
+            <img src={pokemon.smallIcon} alt="icon goes here" />
             <div className="pkmn-name">{pokemon.name}</div>
             <div className="pkmn-id">{pokemon.id}</div>
         </div>
