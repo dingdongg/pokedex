c4bbdb4aa2a7e791c65b1f8c773b6ab60d7458ef
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-07-25 19:52:44 -0600

    Replace components with raw pokemon names

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import Overview from './components/Overview';
 import PokemonEntries from './components/PokemonEntries';
-import React, {useState, useEffect} from 'react';
-import fetchPokemon from './hooks/fetchPokemon';
+import React, {useState} from 'react';
+import fetchPokemon from './hooks/FetchPokemon';
 
 function App() {
 
@@ -14,10 +14,9 @@ function App() {
   // }
   // const [pokedex] = useState(new Pokedex(pokedexConfig));
   const [selectedIndex, setSelectedIndex] = useState(-1);
-  const [numLoaded, setNumLoaded] = useState(4);
 
   const [query, setQuery] = useState("");
-  const [pageNumber, setPageNumber] = useState(1);
+  const [pageNumber, setPageNumber] = useState(23);
 
   const {
     hasMore, 
@@ -27,56 +26,45 @@ function App() {
   } = fetchPokemon(query, pageNumber);
 
   // run only once on reboot
-  useEffect(() => {
-    fetchPokedexData();
-  }, []);
+  // useEffect(() => {
+  //   fetchPokedexData();
+  // }, []);
 
-  async function fetchPokedexData() {
+  // async function fetchPokedexData() {
 
-    let initialData = [];
-    let count;
-    for (let i = 1; i <= 4; i++) {
-      try {
-        await pokedex.getPokemonByName(i)
-          .then((response) => {
-            let parsedInfo = {
-              name: response.name,
-              id: response.id,
-              smallIcon: response.sprites.versions['generation-viii'].icons.front_default,
-              bigIcon: response.sprites.other['official-artwork'].front_default,
-            };
-            initialData = [...initialData, parsedInfo];
-            count = i;
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      } catch (error) {
-        throw error;
-      }
-    }
-    setNumLoaded(count);
-    updatePokemonList(initialData);
-  }
+  //   let initialData = [];
+  //   let count;
+  //   for (let i = 1; i <= 4; i++) {
+  //     try {
+  //       await pokedex.getPokemonByName(i)
+  //         .then((response) => {
+  //           let parsedInfo = {
+  //             name: response.name,
+  //             id: response.id,
+  //             smallIcon: response.sprites.versions['generation-viii'].icons.front_default,
+  //             bigIcon: response.sprites.other['official-artwork'].front_default,
+  //           };
+  //           initialData = [...initialData, parsedInfo];
+  //           count = i;
+  //         })
+  //         .catch((error) => {
+  //           console.error(error);
+  //         });
+  //     } catch (error) {
+  //       throw error;
+  //     }
+  //   }
+  //   setNumLoaded(count);
+  //   updatePokemonList(initialData);
+  // }
 
   return (
     <div className="App">
-      <Overview
-        pokemonList={pokemonList}
-        selectedIndex={selectedIndex}
-      >
-      </Overview>
-      <div className="entries-container">
-        <div className="search-bar-container">
-          <input type="text" className="search-bar" size="25"/>
-        </div>
-        <PokemonEntries
-          pokemonList={pokemonList}
-          updatePokemonList={updatePokemonList}
-          setSelectedIndex={setSelectedIndex}
-        >
-        </PokemonEntries>
-      </div>
+      {pokemonList.map(pokemon => {
+        return <div key={pokemon}>{pokemon}</div>
+      })}
+      <div>{loading ? 'Loading...' : ""}</div>
+      <div>{error ? 'error' : ""}</div>
     </div>
   );
 }
