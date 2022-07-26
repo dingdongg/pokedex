809a1f029cd4c6e711add921a668c55a94f0d748
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-07-25 19:25:18 -0600

    use custom hook fetchPokemon

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import './App.css';
 import Overview from './components/Overview';
 import PokemonEntries from './components/PokemonEntries';
 import React, {useState, useEffect} from 'react';
-import Pokedex from 'pokedex-promise-v2';
+import fetchPokemon from './hooks/fetchPokemon';
 
 function App() {
 
-  const [pokemonList, updatePokemonList] = useState([]);
-  const pokedexConfig = {
-    cache: true,
-    timeout: 5 * 10000,
-    cacheImages: true,
-  }
-  const [pokedex] = useState(new Pokedex(pokedexConfig));
+  // const [pokemonList, updatePokemonList] = useState([]);
+  // const pokedexConfig = {
+  //   cache: true,
+  //   timeout: 5 * 10000,
+  //   cacheImages: true,
+  // }
+  // const [pokedex] = useState(new Pokedex(pokedexConfig));
   const [selectedIndex, setSelectedIndex] = useState(-1);
   const [numLoaded, setNumLoaded] = useState(4);
+
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const {
+    hasMore, 
+    pokemonList,
+    error,
+    loading
+  } = fetchPokemon(query, pageNumber);
 
   // run only once on reboot
   useEffect(() => {
