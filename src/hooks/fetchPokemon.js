77162c260506134507f3c44694203e1abdb57abe
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-07-25 18:46:26 -0600

    Create custom fetch hook file

---
+++ src/hooks/fetchPokemon.js
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from 'react';
+import { Pokedex } from 'pokedex-promise-v2';
+import PokeAPI from 'pokedex-promise-v2';
+
+export default function fetchPokemon() {
+
+    return null;
+}
