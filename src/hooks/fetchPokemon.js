b26ade792f5d7f9c44559b29c157a7179bab0a13
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-07-25 19:21:44 -0600

    set up GET request to PokeAPI

--- src/hooks/fetchPokemon.js
+++ src/hooks/fetchPokemon.js
@@ -3,19 +3,30 @@ import axios from 'axios';
 
 export default function fetchPokemon(query, pageNumber) {
     
+    // public states
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [pokemonList, setPokemonList] = useState([]);
     const [hasMore, setHasMore] = useState(false);
 
+    // private states
     const OFFSET_INCREMENT_FACTOR = 20;
-
-    const [offset, setOffset] = useState(0); // start with the first resource
+    const [offset, setOffset] = useState(20 * (pageNumber - 1)); // start with the first resource
     const [limit, setLimit] = useState(20); // fetch 20 resources at a time
 
+    // TODO: don't worry about the search/filter function right now, focus on the
+    //       infinite loading part
     useEffect(() => {
 
+        axios({
+            method: "GET",
+            url: `https://pokeapi.co/api/v2/pokemon/`,
+            params: {limit, offset},
+        }).then(response => {
+            setOffset(offset + OFFSET_INCREMENT_FACTOR);
+            console.log(response.data);
+        }).catch(error => console.error(error));
 
-    }, []);
+    }, [query, pageNumber]);
 
 }
