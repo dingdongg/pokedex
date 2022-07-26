6509d6176adb21bad71972a7b1e76345972e9d78
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-07-25 20:02:04 -0600

    remove hard-coded values and unneeded comments

--- src/hooks/fetchPokemon.js
+++ src/hooks/fetchPokemon.js
@@ -12,8 +12,8 @@ export default function FetchPokemon(query, pageNumber) {
     // private states
     const OFFSET_INCREMENT_FACTOR = 20;
     const RESULTS_PER_PAGE = 20;
-    const [offset, setOffset] = useState(RESULTS_PER_PAGE * (pageNumber - 1)); // start with the first resource
-    const [limit, setLimit] = useState(RESULTS_PER_PAGE); // fetch 20 resources at a time
+    const [offset, setOffset] = useState(RESULTS_PER_PAGE * (pageNumber - 1));
+    const [limit, setLimit] = useState(RESULTS_PER_PAGE * 2); 
 
     // TODO: don't worry about the search/filter function right now, focus on the
     //       infinite loading part
@@ -28,10 +28,10 @@ export default function FetchPokemon(query, pageNumber) {
             params: {limit, offset},
         }).then(response => {
             setOffset(offset + OFFSET_INCREMENT_FACTOR);
-            console.log(response.data);
+            console.log(response.data.results);
             setPokemonList(pokemonList.concat(...response.data.results.map(pkmn => pkmn.name)));
             setLoading(false);
-            setHasMore(response.data.results.length >= 20);
+            setHasMore(response.data.results.length >= RESULTS_PER_PAGE);
         }).catch(error => {
             setError(true);
             console.error(error);
