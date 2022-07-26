5e6efb10d5c0fca275da2113b49789441bf629cb
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-07-25 19:56:15 -0600

    remove commented out code

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ import fetchPokemon from './hooks/FetchPokemon';
 
 function App() {
 
-  // const [pokemonList, updatePokemonList] = useState([]);
-  // const pokedexConfig = {
-  //   cache: true,
-  //   timeout: 5 * 10000,
-  //   cacheImages: true,
-  // }
-  // const [pokedex] = useState(new Pokedex(pokedexConfig));
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const [query, setQuery] = useState("");
@@ -24,39 +17,6 @@ function App() {
     error,
     loading
   } = fetchPokemon(query, pageNumber);
-
-  // run only once on reboot
-  // useEffect(() => {
-  //   fetchPokedexData();
-  // }, []);
-
-  // async function fetchPokedexData() {
-
-  //   let initialData = [];
-  //   let count;
-  //   for (let i = 1; i <= 4; i++) {
-  //     try {
-  //       await pokedex.getPokemonByName(i)
-  //         .then((response) => {
-  //           let parsedInfo = {
-  //             name: response.name,
-  //             id: response.id,
-  //             smallIcon: response.sprites.versions['generation-viii'].icons.front_default,
-  //             bigIcon: response.sprites.other['official-artwork'].front_default,
-  //           };
-  //           initialData = [...initialData, parsedInfo];
-  //           count = i;
-  //         })
-  //         .catch((error) => {
-  //           console.error(error);
-  //         });
-  //     } catch (error) {
-  //       throw error;
-  //     }
-  //   }
-  //   setNumLoaded(count);
-  //   updatePokemonList(initialData);
-  // }
 
   return (
     <div className="App">
