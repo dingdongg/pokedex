461d400e9419e974cff159044f0c6f21ce9a1b6e
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-07-25 19:53:47 -0600

    set states based on result of api calls

--- src/hooks/fetchPokemon.js
+++ src/hooks/fetchPokemon.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-export default function fetchPokemon(query, pageNumber) {
+export default function FetchPokemon(query, pageNumber) {
     
     // public states
     const [loading, setLoading] = useState(true);
@@ -11,8 +11,9 @@ export default function fetchPokemon(query, pageNumber) {
 
     // private states
     const OFFSET_INCREMENT_FACTOR = 20;
-    const [offset, setOffset] = useState(20 * (pageNumber - 1)); // start with the first resource
-    const [limit, setLimit] = useState(20); // fetch 20 resources at a time
+    const RESULTS_PER_PAGE = 20;
+    const [offset, setOffset] = useState(RESULTS_PER_PAGE * (pageNumber - 1)); // start with the first resource
+    const [limit, setLimit] = useState(RESULTS_PER_PAGE); // fetch 20 resources at a time
 
     // TODO: don't worry about the search/filter function right now, focus on the
     //       infinite loading part
@@ -25,7 +26,13 @@ export default function fetchPokemon(query, pageNumber) {
         }).then(response => {
             setOffset(offset + OFFSET_INCREMENT_FACTOR);
             console.log(response.data);
-        }).catch(error => console.error(error));
+            setPokemonList(pokemonList.concat(...response.data.results.map(pkmn => pkmn.name)));
+            setLoading(false);
+            setHasMore(response.data.results.length >= 20);
+        }).catch(error => {
+            setError(true);
+            console.error(error);
+        });
 
     }, [pageNumber]);
 
