ff5ec6431fc4b6fa1863c728459a6a07ca716f82
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-07-25 19:11:57 -0600

    add more states to custom hook

--- src/hooks/fetchPokemon.js
+++ src/hooks/fetchPokemon.js
@@ -1,12 +1,21 @@
 import React, {useState, useEffect} from 'react';
-import { Pokedex } from 'pokedex-promise-v2';
-import PokeAPI from 'pokedex-promise-v2';
+import axios from 'axios';
 
-export default function fetchPokemon() {
+export default function fetchPokemon(query, pageNumber) {
     
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [pokemonList, setPokemonList] = useState([]);
     const [hasMore, setHasMore] = useState(false);
-    
+
+    const OFFSET_INCREMENT_FACTOR = 20;
+
+    const [offset, setOffset] = useState(0); // start with the first resource
+    const [limit, setLimit] = useState(20); // fetch 20 resources at a time
+
+    useEffect(() => {
+
+
+    }, []);
+
 }
