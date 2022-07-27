a6a8f0150674b9538eb8214cc85ffbc7196e4d0d
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-07-26 19:28:08 -0600

    Prevent last pokemon being fetched first

--- src/hooks/fetchPokemon.js
+++ src/hooks/fetchPokemon.js
@@ -15,6 +15,21 @@ export default function FetchPokemon(query, pageNumber) {
     const [offset, setOffset] = useState(RESULTS_PER_PAGE * (pageNumber - 1));
     const [limit, setLimit] = useState(RESULTS_PER_PAGE); 
 
+    const resolveInOrder = async (batch, last) => {
+        
+        const promises = await Promise.all(batch.map(obj => {
+            return axios.get(obj.url)
+                            .then(response => response.data.name)
+                            .catch(e => console.error(e));
+        }));
+        console.log('first batch', promises);
+
+        const lastName = await axios.get(last.url)
+                                    .then(response => response.data.name)
+                                    .catch(e => console.error(e));
+        console.log('last promise', lastName);
+    }
+
     // TODO: don't worry about the search/filter function right now, focus on the
     //       infinite loading part
     useEffect(() => {
@@ -28,10 +43,17 @@ export default function FetchPokemon(query, pageNumber) {
             params: {limit, offset},
         }).then(response => {
             setOffset(offset + OFFSET_INCREMENT_FACTOR);
-            console.log(response.data.results);
-            setPokemonList(pokemonList.concat(...response.data.results.map(pkmn => pkmn.name)));
-            setLoading(false);
-            setHasMore(response.data.results.length >= RESULTS_PER_PAGE);
+            let results = response.data.results;
+            let firstBatch = results.slice(0, results.length - 1);
+            let lastPokemon = results.slice(-1)[0];
+            setPokemonList(pokemonList.concat(...firstBatch));
+
+            // need API calls to all but the last pokemon on the page to load
+            // before the last pokemon (in no particular order)
+            resolveInOrder(firstBatch, lastPokemon);
+            
+            // setLoading(false);
+            // setHasMore(response.data.results.length >= RESULTS_PER_PAGE);   
         }).catch(error => {
             setError(true);
             console.error(error);
