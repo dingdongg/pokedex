99187a23ee986d7c4acde8c8909ee6f7f2d1b0ec
dingdongg <cs.dg.kim@gmail.com>
2022-08-22 14:18:05 -0700

    forward refs into PokemonEntry

--- src/components/PokemonEntry.js
+++ src/components/PokemonEntry.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import { forwardRef, React } from 'react';
 
-const PokemonEntry = ({pokemon, setSelectedIndex}) => {
+const PokemonEntry = forwardRef(({pokemon, setSelectedIndex}, refs) => {
 
     function updateSelected(e) {
         setSelectedIndex(pokemon.id - 1);
     }
 
     return (
-        <div className={`pkmn-entry ${pokemon.name}`} onClick={updateSelected}>
+        <div ref={refs} className={`pkmn-entry ${pokemon.name}`} onClick={updateSelected}>
             <img src={pokemon.smallIcon} alt={`${pokemon.name}`} />
             <div className="pkmn-name">{pokemon.name}</div>
             <div className="pkmn-id">{pokemon.id}</div>
         </div>
     );
-};
+});
 
 export default PokemonEntry;
