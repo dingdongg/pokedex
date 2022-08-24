5b6f1efbe5c938af992d233993b15e8035a3219b
dingdongg <cs.dg.kim@gmail.com>
2022-08-23 18:23:46 -0700

    render appropriate type SVG

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,4 +1,11 @@
 import React from 'react';
+const reqSvgs = require.context('../icons', true, /\.svg$/);
+const paths = reqSvgs.keys();
+
+const iconsRepo = paths.reduce((images, p) => {
+    images[p.slice(2)] = reqSvgs(p);
+    return images;
+}, {});
 
 const Overview = ({pokemonList, selectedIndex}) => {
     if (selectedIndex === -1) {
@@ -14,8 +21,16 @@ const Overview = ({pokemonList, selectedIndex}) => {
             <img src={pkmn.bigIcon} alt="big icon of pokemon" />
             <div>{pkmn.name}</div>
             <div className="types-container">
-                <span>{pkmn.typeOne}</span>
-                <span>{(pkmn.typeTwo ? ` || ${pkmn.typeTwo}` : '')}</span>
+                <img 
+                    className={`icon ${pkmn.typeOne}`} 
+                    src={iconsRepo[`${pkmn.typeOne}.svg`]} 
+                    alt={pkmn.typeOne} />
+                {(pkmn.typeTwo ?
+                    <img 
+                        className={`icon ${pkmn.typeTwo}`}
+                        src={iconsRepo[`${pkmn.typeTwo}.svg`]}
+                        alt={pkmn.typeTwo} /> :
+                    '')}
             </div>
         </div>
     );
